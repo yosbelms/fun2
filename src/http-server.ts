0d1b357f1f792e38567fe683d1c7844e17e5cec4
Yosbel Sardinas <yosbel.sardinas@Yosbel-Sardinas-Mac.local>
2019-10-04 03:42:01 -0300

    add optional parseArgs to http middlewares

--- src/http-server.ts
+++ src/http-server.ts
@@ -5,31 +5,34 @@ interface SourceArgsContainer {
   args?: string
 }
 
-export const handleHttpRequest = async (
-  runner: Runner,
-  method: string,
-  query: SourceArgsContainer = {},
-  body: SourceArgsContainer = {}
-) => {
-  let source
-  let args
-
-  switch (method.toUpperCase()) {
-    case 'GET':
-      source = query.source
-      args = query.args
-      break
-    case 'POST':
-      source = query.source
-      args = body.args
-      break
-  }
-
-  const parsedArgs = (
+const parseJson = (args: any) => {
+  return (
     typeof args === 'string'
       ? JSON.parse(args)
       : void 0
   )
+}
+
+export const handleHttpRequest = async (
+  runner: Runner,
+  method: string,
+  query: SourceArgsContainer = {},
+  body: SourceArgsContainer = {},
+  parseArgs: Function = parseJson
+) => {
+  let source = query.source
+  let args
+
+  switch (method.toUpperCase()) {
+    case 'GET':
+      args = query.args
+      break
+    case 'POST':
+      args = body.args
+      break
+  }
+
+  const parsedArgs = parseArgs(args)
 
   return await (runner as Runner).run(
     source || '',
@@ -37,27 +40,35 @@ export const handleHttpRequest = async (
   )
 }
 
-export const createExpressMiddleware = (runner: Runner = createRunner()) => {
+export const createExpressMiddleware = (
+  runner: Runner = createRunner(),
+  parseArgs?: Function
+) => {
   return async (request: any, response: any, next: Function) => {
     const result = await handleHttpRequest(
       runner,
       request.method,
       request.query,
-      request.body
+      request.body,
+      parseArgs,
     )
     response.send(result)
     next()
   }
 }
 
-export const createKoaMiddleware = (runner: Runner = createRunner()) => {
+export const createKoaMiddleware = (
+  runner: Runner = createRunner(),
+  parseArgs?: Function
+) => {
   return async (ctx: any, next: Function) => {
     const { request, response } = ctx
     const result = await handleHttpRequest(
       runner,
       request.method,
       request.query,
-      request.body
+      request.body,
+      parseArgs
     )
     response.body = result
     next()
